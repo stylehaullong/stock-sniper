f38a938b76a37f317ddd9d162f51b049744858ec
long-nguyen-zeal <166643811+long-nguyen-zeal@users.noreply.github.com>
2026-02-22 23:21:27 -0800

    fix middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,12 +14,12 @@ export async function middleware(request: NextRequest) {
         getAll() {
           return request.cookies.getAll();
         },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
+        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
+          cookiesToSet.forEach(({ name, value }: { name: string; value: string }) => request.cookies.set(name, value));
           supabaseResponse = NextResponse.next({
             request,
           });
-          cookiesToSet.forEach(({ name, value, options }) =>
+          cookiesToSet.forEach(({ name, value, options }: { name: string; value: string; options?: any }) =>
             supabaseResponse.cookies.set(name, value, options)
           );
         },
